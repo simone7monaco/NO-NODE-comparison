5a700121f7db0aad8be87262c6f4d6ad6225dfa2
frogeye00 <CLEMTT00E08G371P@studium.unict.it>
2024-11-22 14:26:26 +0100

    Update EGNO_sweep.py

--- EGNO/EGNO_sweep.py
+++ EGNO/EGNO_sweep.py
@@ -36,6 +36,6 @@ def train(config=None):
         main(config)
         
 
-wandb.agent(sweep_id, train) #MAX_RUN
 sweep_id = wandb.sweep(sweep_config, project="EGNO-sweep-new")
+wandb.agent(sweep_id, train) #MAX_RUN
 
